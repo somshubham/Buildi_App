b5006aa3eed76c2a01e8dc150f5b11b30352624c
somshubham <somshubhams@gmail.com>
2017-01-13 20:41:58 +0530

    Login in backg....

--- app/src/main/java/com/mobileapp/buildi/buildi/login/LoginFragment.java
+++ app/src/main/java/com/mobileapp/buildi/buildi/login/LoginFragment.java
@@ -16,10 +16,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONObject;
 
 import com.mobileapp.buildi.buildi.R;
@@ -33,11 +36,14 @@ import com.squareup.okhttp.Response;
 
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -78,7 +84,9 @@ public class LoginFragment extends Fragment {
                 Log.i("Data", "email=" + emailValue + " pass=" + passwordValue);
 
 
-                login(emailValue, passwordValue);
+               // login(emailValue, passwordValue);
+                AsyncT asyncT = new AsyncT();
+                asyncT.execute();
             }
         });
 
@@ -179,77 +187,186 @@ public class LoginFragment extends Fragment {
 
 
 
-    private void login(String email,String password) {
 
-        InputStream inputStream = null;
-        String result = "";
-        try {
 
-            URL url = new URL("https://warm-depths-10529.herokuapp.com/login");
-            // 1. create HttpClient
+
+
+
+
+
+
+
+
+
+//
+//    private void login(String email,String password) {
+//
+//        InputStream inputStream = null;
+//        String result = "";
+//        try {
+//
+//            URL url = new URL("https://warm-depths-10529.herokuapp.com/login");
+//            // 1. create HttpClient
+//            HttpClient httpclient = new DefaultHttpClient();
+//
+//            // 2. make POST request to the given URL
+//            HttpPost httpPost = new HttpPost(String.valueOf(url));
+//
+//            String json = "";
+//
+//            // 3. build jsonObject
+//            JSONObject jsonObject = new JSONObject();
+//            jsonObject.accumulate("username", ""+email);
+//            jsonObject.accumulate("password", ""+password);
+//
+//            // 4. convert JSONObject to JSON to String
+//            json = jsonObject.toString();
+//
+//            // ** Alternative way to convert Person object to JSON string usin Jackson Lib
+//            // ObjectMapper mapper = new ObjectMapper();
+//            // json = mapper.writeValueAsString(person);
+//
+//            // 5. set json to StringEntity
+//            StringEntity se = new StringEntity(json);
+//
+//            // 6. set httpPost Entity
+//            httpPost.setEntity(se);
+//
+//            // 7. Set some headers to inform server about the type of the content
+//            httpPost.setHeader("Accept", "application/json");
+//            httpPost.setHeader("Content-type", "application/json");
+//
+//            // 8. Execute POST request to the given URL
+//            HttpResponse httpResponse = httpclient.execute(httpPost);
+//
+//            // 9. receive response as inputStream
+//            inputStream = httpResponse.getEntity().getContent();
+//
+//            // 10. convert inputstream to string
+//            if(inputStream != null) {
+//                result = convertInputStreamToString(inputStream);
+//                Log.i("data",""+result);
+//            }
+//            else {
+//                result = "Did not work!";
+//            }
+//
+//        } catch (Exception e) {
+//            Log.d("InputStream", e.getLocalizedMessage());
+//        }
+//
+//    }
+//
+//
+//
+//    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+//        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+//        String line = "";
+//        String result = "";
+//        while((line = bufferedReader.readLine()) != null)
+//            result += ""+line;
+//
+//        inputStream.close();
+//        return result;
+//
+//    }
+//
+
+
+    // ###############################################
+
+    String responseServer;
+
+    /* Inner class to get response */
+    class AsyncT extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
             HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost("https://warm-depths-10529.herokuapp.com/login");
 
-            // 2. make POST request to the given URL
-            HttpPost httpPost = new HttpPost(String.valueOf(url));
+            try {
 
-            String json = "";
+                JSONObject jsonobj = new JSONObject();
 
-            // 3. build jsonObject
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.accumulate("username", ""+email);
-            jsonObject.accumulate("password", ""+password);
+                jsonobj.put("username", "somshubhamsandroid@gmail.com");
+                jsonobj.put("password", "12345");
 
-            // 4. convert JSONObject to JSON to String
-            json = jsonObject.toString();
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+                nameValuePairs.add(new BasicNameValuePair("req", jsonobj.toString()));
+           Log.i("res",""+jsonobj.toString());
+                Log.e("mainToPost", "mainToPost" + nameValuePairs.toString());
 
-            // ** Alternative way to convert Person object to JSON string usin Jackson Lib
-            // ObjectMapper mapper = new ObjectMapper();
-            // json = mapper.writeValueAsString(person);
+                // Use UrlEncodedFormEntity to send in proper format which we need
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
-            // 5. set json to StringEntity
-            StringEntity se = new StringEntity(json);
+                // Execute HTTP Post Request
+                HttpResponse response = httpclient.execute(httppost);
+                InputStream inputStream = response.getEntity().getContent();
+                InputStreamToStringExample str = new InputStreamToStringExample();
+                responseServer = str.getStringFromInputStream(inputStream);
+                Log.e("response", "response -----" + responseServer);
 
-            // 6. set httpPost Entity
-            httpPost.setEntity(se);
 
-            // 7. Set some headers to inform server about the type of the content
-            httpPost.setHeader("Accept", "application/json");
-            httpPost.setHeader("Content-type", "application/json");
-
-            // 8. Execute POST request to the given URL
-            HttpResponse httpResponse = httpclient.execute(httpPost);
-
-            // 9. receive response as inputStream
-            inputStream = httpResponse.getEntity().getContent();
-
-            // 10. convert inputstream to string
-            if(inputStream != null) {
-                result = convertInputStreamToString(inputStream);
-                Log.i("data",""+result);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            else {
-                result = "Did not work!";
-            }
+            return null;
+        }
 
-        } catch (Exception e) {
-            Log.d("InputStream", e.getLocalizedMessage());
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            //txt.setText(responseServer);
+        }
+    }
+
+    public static class InputStreamToStringExample {
+
+        public static void main(String[] args) throws IOException {
+
+            // intilize an InputStream
+            InputStream is =
+                    new ByteArrayInputStream("file content..blah blah".getBytes());
+
+            String result = getStringFromInputStream(is);
+
+            System.out.println(result);
+            System.out.println("Done");
+
+        }
+
+        // convert InputStream to String
+        private static String getStringFromInputStream(InputStream is) {
+
+            BufferedReader br = null;
+            StringBuilder sb = new StringBuilder();
+
+            String line;
+            try {
+
+                br = new BufferedReader(new InputStreamReader(is));
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return sb.toString();
         }
 
     }
 
 
-
-    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
-        String line = "";
-        String result = "";
-        while((line = bufferedReader.readLine()) != null)
-            result += ""+line;
-
-        inputStream.close();
-        return result;
-
-    }
 
 
 
